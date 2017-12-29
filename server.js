682d1615e86b79f5ffa3b7adf4fbfc42c1aa7364
Cody Shaner <cshaner04@gmail.com>
2017-12-29 16:30:14 -0700

    remove

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const path     = require("path");
+
+app.use(cors());
