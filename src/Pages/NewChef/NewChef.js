25294a62510c5a108e43bb2295cd6be947c63a00
Cody Shaner <cshaner04@gmail.com>
2018-01-01 00:52:42 -0700

    added form css 1

--- src/Pages/NewChef/NewChef.js
+++ src/Pages/NewChef/NewChef.js
@@ -10,8 +10,22 @@ class NewChef extends Component {
                 
                 <div className="MainWindow">
                     <form>
-                        <input type="text" name="email" placeholder="Email" />
+                        <h6>First Name</h6>
                         <input type="text" name="firstName" />
+                        <h6>Last Name</h6>
+                        <input type="text" name="lastName" />
+                        <h6>Email</h6>
+                        <input type="email" name="email" />
+                        <h6>Dob</h6>
+                        <input type="date" name="bday" />
+
+                        <h6>For these questions, answer on a scale of 0-10 
+                        How comfortable are you performing infront of others?</h6>
+                        <input type="range" name="perform" min="0" max="10" />
+                        Points:
+                        <input type="range" name="points" min="0" max="10" />
+                        Points:
+                        <input type="range" name="points" min="0" max="10" />
                     </form>
                 </div>
 
